eba061630b8f807577340715fdac21a3315c5bc7
medc-0 <moonisontop76@gmail.com>
2026-01-16 15:30:57 +0100

    refactor: update PaintApp to use a canvas surface and improve rendering logic

--- paint.cpp
+++ paint.cpp
@@ -1,22 +1,22 @@
 #define SDL_MAIN_HANDLED
 #include "paint.h"
 
-#define START_COLOR 0xFF0000
-
-static const int WIDTH = 900;
-static const int HEIGHT = 600;
-
 PaintApp::PaintApp()
-    : window(nullptr), surface(nullptr), color(START_COLOR) {
+    : color(0xFF000000) {
     SDL_Init(SDL_INIT_VIDEO);
     window = SDL_CreateWindow("Ultra Pain(t)", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
-    surface = SDL_GetWindowSurface(window);
+    screen = SDL_GetWindowSurface(window);
+
+    canvas = SDL_CreateRGBSurfaceWithFormat(0, CANVAS_W, CANVAS_H, 32, SDL_PIXELFORMAT_ARGB8888);
+    SDL_FillRect(canvas, nullptr, 0xFFFFFFFF);
 }
 
 PaintApp::~PaintApp() {
+    if (canvas) SDL_FreeSurface(canvas);
     if (window) SDL_DestroyWindow(window);
     SDL_Quit();
 }
+
 /*
     Returns true if (x, y) is inside the color palette area.
 */
@@ -41,10 +41,10 @@ void PaintApp::pick_color(int x)
 void PaintApp::clear_canvas(Uint32 resetcolor) 
 {
     // std::cout << "key:C\n";
-    SDL_Rect fillpixel = {0, PALETTE_HEIGHT, WIDTH, HEIGHT - PALETTE_HEIGHT};
-    SDL_FillRect(surface, &fillpixel, resetcolor);
-    // draw_palette();
-    SDL_UpdateWindowSurface(window);
+    // SDL_Rect fillpixel = {0, PALETTE_HEIGHT, WIDTH, HEIGHT - PALETTE_HEIGHT};
+    // SDL_FillRect(canvas, &fillpixel, resetcolor);
+    SDL_FillRect(canvas, nullptr, resetcolor);
+    // SDL_UpdateWindowSurface(window);
 }
 
 /*  
@@ -53,10 +53,10 @@ void PaintApp::clear_canvas(Uint32 resetcolor)
 void PaintApp::fill_canvas()
 {
     // std::cout << "key:F\n";
-    SDL_Rect fpixel = {0, 20, WIDTH, HEIGHT - PALETTE_HEIGHT};
-    SDL_FillRect(surface, &fpixel, color);
-    draw_palette();
-    SDL_UpdateWindowSurface(window);
+    // SDL_Rect fpixel = {0, 20, WIDTH, HEIGHT - PALETTE_HEIGHT};
+    SDL_FillRect(canvas, nullptr, color);
+    // draw_palette();
+    // SDL_UpdateWindowSurface(window);
 }
 
 /*
@@ -64,26 +64,26 @@ void PaintApp::fill_canvas()
 */
 void PaintApp::draw_palette()
 {
-    SDL_Rect rect;
-
+    SDL_Rect r; 
     for (int i = 0; i < COLOR_PALETTE_SIZE; ++i) {
-        rect = { i * COLOR_RECT_SIZE, 0, COLOR_RECT_SIZE, COLOR_RECT_SIZE };
-        SDL_FillRect(surface, &rect, color_palette[i]);
+        r = {i * COLOR_RECT_SIZE, 0, COLOR_RECT_SIZE, COLOR_RECT_SIZE};
+        SDL_FillRect(screen, &r, color_palette[i]);
     }
 }
 
 /*
     Draws a filled circle using squared distance checks
 */
+
 void PaintApp::draw_circle(int cx, int cy, int radius, Uint32 col)
 {
     int r2 = radius * radius;
-    SDL_Rect pixel = {0, 0, 1, 1};
+    SDL_Rect pixel{0, 0, 1, 1};
 
     for (int x = cx - radius; x <= cx + radius; ++x) {
         for (int y = cy - radius; y <= cy + radius; ++y) {
-            if (x < 0 || y < PALETTE_HEIGHT ||
-                x >= surface->w || y >= surface->h)
+
+            if (x < 0 || y < 0 || x >= canvas->w || y >= canvas->h)
                 continue;
 
             int dx = x - cx;
@@ -92,103 +92,92 @@ void PaintApp::draw_circle(int cx, int cy, int radius, Uint32 col)
             if (dx * dx + dy * dy <= r2) {
                 pixel.x = x;
                 pixel.y = y;
-                SDL_FillRect(surface, &pixel, col);
+                SDL_FillRect(canvas, &pixel, col);
             }
         }
     }
 }
 
-void PaintApp::run() {
+void PaintApp::render() 
+{
+    SDL_Rect src = { cam_x, cam_y, WIDTH, HEIGHT };
+    SDL_Rect dst = { 0, PALETTE_HEIGHT, WIDTH, HEIGHT - PALETTE_HEIGHT };
 
+    SDL_BlitSurface(canvas, &src, screen, &dst);
+    draw_palette();
+    SDL_UpdateWindowSurface(window);
+}
+
+void PaintApp::run()
+{
     bool running = true;
     bool drawing = false;
-    bool needs_redraw = true;
 
     SDL_Event event;
-
-    int mouse_x = 0;
-    int mouse_y = 0;
+    int mouse_x = 0, mouse_y = 0;
     int brush_size = START_RADIUS;
 
     Uint32 frame_delay = 1000 / TARGET_FPS;
-    draw_palette();
-    SDL_UpdateWindowSurface(window);
 
     while (running) {
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
 
-                case SDL_QUIT:
-                    running = false;
-                    break;
+            case SDL_QUIT:
+                running = false;
+                break;
 
-                case SDL_MOUSEMOTION:
-                    mouse_x = event.motion.x;
-                    mouse_y = event.motion.y;
-                    break;
+            case SDL_MOUSEMOTION:
+                mouse_x = event.motion.x;
+                mouse_y = event.motion.y;
+                break;
 
-                case SDL_MOUSEBUTTONDOWN:
-                    mouse_x = event.button.x;
-                    mouse_y = event.button.y;
+            case SDL_MOUSEBUTTONDOWN:
+                if (inside_color_palette(mouse_x, mouse_y)) {
+                    pick_color(mouse_x);
+                } else {
+                    drawing = true;
+                }
+                break;
 
-                    if (inside_color_palette(mouse_x, mouse_y)) {
-                        pick_color(mouse_x);
-                        needs_redraw = true;
-                    }
-                    else {
-                        drawing = true;
-                    }
-                    break;
+            case SDL_MOUSEBUTTONUP:
+                drawing = false;
+                break;
 
-                case SDL_MOUSEBUTTONUP:
-                    drawing = false;
-                    break;
+            case SDL_MOUSEWHEEL:
+                brush_size += (event.wheel.y > 0) ? 1 : -1;
+                brush_size = std::max(1, brush_size);
+                break;
 
-                case SDL_KEYDOWN:
-                    if (event.key.keysym.sym == SDLK_ESCAPE) running = false;
-                    if (event.key.keysym.sym == SDLK_c) clear_canvas(0x00000000);
-                    if (event.key.keysym.sym == SDLK_f) fill_canvas();
-                    break;
+            case SDL_KEYDOWN:
+                if (event.key.keysym.sym == SDLK_ESCAPE) running = false;
+                if (event.key.keysym.sym == SDLK_c) clear_canvas(0xFFFFFFFF);
 
-                case SDL_MOUSEWHEEL:
-                    brush_size += (event.wheel.y > 0) ? 1 : -1;
-                    if (brush_size < 1) brush_size = 1;
-                    std::cout << "Brush size: " << brush_size << '\n';
-                    break;
+                if (event.key.keysym.sym == SDLK_a) cam_x -= 40;
+                if (event.key.keysym.sym == SDLK_d) cam_x += 40;
+                if (event.key.keysym.sym == SDLK_w) cam_y -= 40;
+                if (event.key.keysym.sym == SDLK_s) cam_y += 40;
+
+                cam_x = std::clamp(cam_x, 0, CANVAS_W - WIDTH);
+                cam_y = std::clamp(cam_y, 0, CANVAS_H - HEIGHT);
+                break;
             }
         }
 
         if (drawing && mouse_y >= PALETTE_HEIGHT) {
-            draw_circle(mouse_x, mouse_y, brush_size, color);
-            needs_redraw = true;
+            int world_x = mouse_x + cam_x;
+            int world_y = mouse_y + cam_y - PALETTE_HEIGHT;
+            draw_circle(world_x, world_y, brush_size, color);
         }
 
-        if (needs_redraw) {
-            draw_palette();
-            SDL_UpdateWindowSurface(window);
-            needs_redraw = false;
-        }
-
+        render();
         SDL_Delay(frame_delay);
     }
 }
-
 
 int main()
 {
     PaintApp app;
     app.run();
-    return EXIT_SUCCESS;
+    return 0;
 }
-
-
-
-
-// Coding today:
-/*
-    - Get rid of warnings
-    - Expandable canvas size
-    - Save / Open feature (maybe too advanced)
-    - Better UI and Compatibility
-    - More colors?
-*/
--- paint.h
+++ paint.h
@@ -3,29 +3,41 @@
 
 #include <SDL2/SDL.h>
 #include <iostream>
+#include <algorithm>
 
 class PaintApp {
-    public:
-        PaintApp();
-        ~PaintApp();
-        void run();
+public:
+    PaintApp();
+    ~PaintApp();
+    void run();
 
-        private:
-        SDL_Window* window;
-        SDL_Surface* surface;
-        bool inside_color_palette(int x, int y);
-        void pick_color(int x);
-        void draw_palette();
-        void draw_circle(int cx, int cy, int radius, Uint32 col);
-        void clear_canvas(Uint32 resetcolor);
-        void fill_canvas();
-        static const int COLOR_PALETTE_SIZE = 8;
-        static const int COLOR_RECT_SIZE = 20;
-        static const int PALETTE_HEIGHT = COLOR_RECT_SIZE;
-        static const int START_RADIUS = 5;
-        static const int TARGET_FPS = 60;
-        Uint32 color;
-        Uint32 color_palette[COLOR_PALETTE_SIZE] = {0x000000, 0xFFFFFF, 0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0x00FFFF, 0xFF00FF};
+private:
+    SDL_Window* window = nullptr;
+    SDL_Surface* screen = nullptr;
+
+    SDL_Surface* canvas = nullptr;
+    int cam_x = 0;
+    int cam_y = 0;
+
+    Uint32 color;
+
+    static const int WIDTH = 900;
+    static const int HEIGHT = 600;
+    static const int CANVAS_W = 8000;
+    static const int CANVAS_H = 8000;
+    static const int COLOR_PALETTE_SIZE = 8;
+    static const int COLOR_RECT_SIZE = 20;
+    static const int PALETTE_HEIGHT = COLOR_RECT_SIZE;
+    static const int START_RADIUS = 5;
+    static const int TARGET_FPS = 60;
+    Uint32 color_palette[COLOR_PALETTE_SIZE] = {0xFF000000, 0xFFFFFFFF, 0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFF00, 0xFF00FFFF, 0xFFFF00FF};
+    bool inside_color_palette(int x, int y);
+    void pick_color(int x);
+    void draw_palette();
+    void draw_circle(int cx, int cy, int radius, Uint32 col);
+    void fill_canvas();
+    void clear_canvas(Uint32 resetcolor);
+    void render();
 };
 
 #endif
