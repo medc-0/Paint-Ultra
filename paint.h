614a2b021c4acfc5bb785e9c70ca8da5728ef3c5
medc-0 <moonisontop76@gmail.com>
2026-01-16 16:06:59 +0100

    refactor: default brush_size

--- paint.h
+++ paint.h
@@ -24,12 +24,12 @@ private:
 
     static const int WIDTH = 900;
     static const int HEIGHT = 600;
-    static const int CANVAS_W = 8000;
-    static const int CANVAS_H = 8000;
+    static const int CANVAS_W = 4000;
+    static const int CANVAS_H = 4000;
     static const int COLOR_PALETTE_SIZE = 8;
     static const int COLOR_RECT_SIZE = 20;
     static const int PALETTE_HEIGHT = COLOR_RECT_SIZE;
-    static const int START_RADIUS = 5;
+    static const int START_RADIUS = 10;
     static const int TARGET_FPS = 60;
     Uint32 color_palette[COLOR_PALETTE_SIZE] = {0xFF000000, 0xFFFFFFFF, 0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFF00, 0xFF00FFFF, 0xFFFF00FF};
     bool inside_color_palette(int x, int y);
